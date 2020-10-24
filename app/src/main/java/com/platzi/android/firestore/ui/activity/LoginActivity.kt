7c99f94354678241a2a37538b9cc287109c0cb86
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-24 11:28:27 -0500

    Estructura y creación de Datos en Firestore: Correcion de Login Activity

--- app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/LoginActivity.kt
@@ -2,12 +2,19 @@ package com.platzi.android.firestore.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.platzi.android.firestore.R
-import kotlinx.android.synthetic.main.activity_trader.*
+import com.platzi.android.firestore.model.User
+import com.platzi.android.firestore.network.Callback
+import com.platzi.android.firestore.network.FirestoreService
+import com.platzi.android.firestore.network.USERS_COLLECTION_NAME
+import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.Exception
 
 /**
  * @author Santiago Carrillo
@@ -26,27 +33,55 @@ class LoginActivity : AppCompatActivity() {
     //Creamos la instancia del modulo de autenticacion de firebase FirebaseAuth
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
 
+    //Creamos una referencia al servicio creado en network
+    lateinit var firestoreService: FirestoreService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //Intaciamos la intacia de FirebaseFirestore en FirestoreService mismo que lo pasamos como constructor
+        firestoreService = FirestoreService(FirebaseFirestore.getInstance())
     }
 
 
     fun onStartClicked(view: View) {
+        view.isEnabled = false //Desabilitamos el boton OnStart con isEnabled
         auth.signInAnonymously()
             //NOTA: Habilitar en FIREBASE la Authentication - Anónimo
+            //NOTA: Habilitar en FIREBASE en Rules los permios de escritura allow read, write: if true;
             //Agregamos un listener(addOnCompleteListener) que nos estara notificando si esta operacion fue exitosa o no
             .addOnCompleteListener {task ->//Retorna una tarea si es exitosa o no
                 if (task.isSuccessful) {
-                   val username = usernameTextView.text.toString()//obtenemos el valor del username
+                   val username = username.text.toString()//obtenemos el valor del username
+
+                   val user = User()
+                    user.username = username
+
                     //Lo enviamos al mainActivity el username
-                    startMainActivity(username)
+                    saveUserAndStartMainActivity(user, view)//La vista es para utilizarla en el showErrorMessage que es lo que recibe
                 } else {//En caso de que no sea exitosa la conexion
                     showErrorMessage(view)//Mostramos un mensaje de error en caso de que caiga en este supuesto
+                    //En caso de que ocurra un error habilitamos el boton
+                    view.isEnabled = true
                 }
             }
-        startMainActivity("Santiago")
 
+    }
+
+    private fun saveUserAndStartMainActivity(user: User, view: View) {
+        firestoreService.setDocument(user, USERS_COLLECTION_NAME, user.username,
+            object : Callback<Void> {//Nos muestra dos ecenarios
+                override fun onSuccess(result: Void?) {//En caso de que se a exitoso
+                    startMainActivity(user.username)//comenzamos la activadad principal
+                }
+
+                override fun onFailed(exception: Exception) {//En caso de que exista un error
+                    showErrorMessage(view)//Mostramos un mensaje de error
+                    Log.e(TAG, "error", exception)
+                    view.isEnabled = true //Habilitamos el boton OnStart con isEnabled
+                }
+
+            })
     }
 
     private fun showErrorMessage(view: View) {
